441279a5381b62193498e7f50e4bc40dae07ff4f
nadiagarrido <garridonadia@gmail.com>
2014-11-16 22:38:10 -0300

    mejoras en el código

--- NumerosCirculares/src/FuncionesComunes.java
+++ NumerosCirculares/src/FuncionesComunes.java
@@ -5,25 +5,30 @@ public class FuncionesComunes {
 	public static boolean esPrimo(int numero)
 	{
 		int j=0, div=0,raiz=0;
+		String num=Integer.toString(numero);
+		
 		if (numero!=0 & numero!=1) //Verifica que el número sea distinto de 0 y 1 que no son considerados números primos
-		{		
-			div=0; // variable que determina la cantidad de divisores que tiene el número
-			raiz=(int)sqrt(numero); //Obtiene la raíz del número
-			
-			for (j=1;j<=raiz;j++) 
-			{ 
-			   if (numero%j==0) // Si el resto es 0 suma 1 a la variable div
-			   div++;
-			}
-			if (div<=1)
+		{
+			if (num.length()>1) //Verifica que el número tenga más de dos dígitos sino no tiene números circulares
 			{
-				return true; //Si la cant de divisores es menor o igual a 1 es un número primo
+				div=0; // variable que determina la cantidad de divisores que tiene el número
+				raiz=(int)sqrt(numero); //Obtiene la raíz del número
+				
+				for (j=1;j<=raiz;j++) 
+				{ 
+				   if (numero%j==0) // Si el resto es 0 suma 1 a la variable div
+				   div++;
+				}
+				if (div<=1)
+				{
+					return true; //Si la cant de divisores es menor o igual a 1 es un número primo
+				}
+				else
+				{
+					return false;
+				}
 			}
-			else
-			{
-				return false;
-			}
-			
+			else {return false;}
 		}
 		else {return false;}
 			
